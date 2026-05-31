dd04f7701ba1f1a80e4fe8afc1e29d2b3c8293bb
DHLinh11 <daodienhoanglinh@gmail.com>
2026-05-31 15:50:13 +0700

    createPool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
+const connection = require('../config/database');
+
 const getHomePage = (req, res) => {
-	res.send('Hello World!');
+	// process data
+	//call model
+	let users = [];
+	connection.query('select * from Users u', function (err, results, fields) {
+		users = results;
+		console.log('>>>Check Results ', results);
+		console.log('>>>check user ', users);
+		res.send(JSON.stringify(users));
+	});
 };
 
 const getDhl = (req, res) => {
--- src/sever.js
+++ src/sever.js
@@ -19,7 +19,6 @@ app.use('/', webRoutes);
 // simple query
 connection.query('select * from Users u', function (err, results, fields) {
 	console.log(results);
-	console.log(fields);
 });
 
 app.listen(port, hostname, () => {
